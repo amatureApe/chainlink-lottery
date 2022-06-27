759106d85070c7221061da9551915a67e812b7d7
amatureApe <amatureApe@gmail.com>
2022-06-27 18:40:52 -0500

    fix typo

--- scripts/mockOffChain.js
+++ scripts/mockOffChain.js
@@ -19,7 +19,7 @@ async function mockKeepers() {
 
 async function mockVrf(requestId, raffle) {
   console.log("Are we on a local network? Ok let's pretend...");
-  const vrfCoordinatorV2Mock = awiat ethers.getContract("VRFCoordinatorV2Mock");
+  const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
   await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, raffle.address);
   console.log("Responded!");
   const recentWinner = await raffle.getRecentWinner();
