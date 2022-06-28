095f457218b59922b11fbc778d612d6c44777c52
amatureApe <amatureApe@gmail.com>
2022-06-27 19:55:07 -0500

    mockvrf fixes

--- scripts/mockOffChain.js
+++ scripts/mockOffChain.js
@@ -1,15 +1,16 @@
-const { ethers, network } = require("hardhat");
+const { ethers, network, getChainId } = require("hardhat");
 
 async function mockKeepers() {
   const raffle = await ethers.getContract("Raffle");
   const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""));
+  const chainId = await getChainId();
   const { upkeepNeeded } = await raffle.callStatic.checkUpkeep(checkData);
   if (upkeepNeeded) {
     const tx = await raffle.performUpkeep(checkData);
     const txReceipt = await tx.wait(1);
     const requestId = txReceipt.events[1].args.requestId;
     console.log(`Performed upkeep with RequestId: ${requestId}`);
-    if (network.config.chainId == 31337) {
+    if (chainId == 31337) {
       await mockVrf(requestId, raffle)
     }
   } else {
